6e312d6f80f60b942d3f92f0790955e91fcbf356
Madhav Karavadiya <madhavpatel236@gmail.com>
2024-12-26 18:27:52 +0530

    make a login page

--- src/componenets/Login.jsx
+++ src/componenets/Login.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React from "react";
 
 function Login() {
-
   const handleClick = async () => {
     const client_id = "ad72028e41d549128a554688bdf2acb0";
     const redirect_uri = "https://madhav-music-player.netlify.app/";
@@ -21,10 +20,20 @@ function Login() {
   };
 
   return (
-    <div>
-      <button onClick={handleClick} className='bg-red-400 flex justify-center items-center'> Click Here </button>
+    <div className="bg-red-950 justify-center flex w-screen h-screen text-white">
+    <div className="  flex-col flex  items-center justify-center">
+      <h1 className="mb-3 text-3xl font-medium font-serif"> Login for use the App</h1>
+      <button
+        onClick={handleClick}
+        className="bg-red-300 py-2 text-black font-serif font-semibold text-lg hover:bg-red-400 hover:scale-110 px-20 rounded-lg"
+      >
+        {" "}
+        Login{" "}
+      </button>
     </div>
-  )
+    </div>
+      
+  );
 }
 
-export default Login
+export default Login;
